95b3fe11b38f11416d3d60d816d43cf7e316fb90
Thiago Felipe <thiago.felipe@tutanota.com>
2022-03-05 10:48:33 -0400

    Changing the handlers interfaces

--- frontend/src/lib/interface.ts
+++ frontend/src/lib/interface.ts
@@ -1,7 +1,17 @@
-import type { RequestHandler as GenericRequestHandler } from "@sveltejs/kit";
+import type {
+  RequestHandler as GenericRequestHandler,
+  RequestHandlerOutput as GenericRequestHandlerOutput
+} from "@sveltejs/kit";
 
 export type RequestHandler<Body = unknown> = GenericRequestHandler<
   Record<string, string>,
+
+  //@ts-expect-error: body not readable
+  Body
+>;
+
+
+export type RequestHandlerOutput<Body = unknown> = GenericRequestHandlerOutput<
 
   //@ts-expect-error: body not readable
   Body
--- frontend/src/routes/api/todo/index.ts
+++ frontend/src/routes/api/todo/index.ts
@@ -1,21 +1,24 @@
-import type { Todo, RequestHandler } from "$lib/interface";
+import type { Todo, RequestHandlerOutput } from "$lib/interface";
 import { v4 as uuidv4 } from "uuid";
 
 const todos: Todo[] = [];
 
-export const get: RequestHandler = () => ({
-  status: 200,
-  body:   todos
-});
+export function get(): RequestHandlerOutput<Todo[]> {
+  return {
+    status: 200,
+    body:   todos
+  };
+}
 
+export async function post({ request }: {request: Request}) {
+  const body = await request.json();
 
-export const post: RequestHandler = async (event) => {
-  const body = await event.request.json();
   todos.push({
     id:          uuidv4(),
     done:        false,
     title:       body.title,
     description: body.description
   });
+
   return { status: 200 };
-};
+}
