ea99fbe1d6288a400876df3f04d8bf9dfc64744e
Thiago Felipe <thiago.felipe@tutanota.com>
2022-03-04 20:01:01 -0400

    Creating the basic style

--- frontend/src/lib/task.svelte
+++ frontend/src/lib/task.svelte
@@ -2,20 +2,55 @@
   export let id: string,
              title: string,
              description: string,
-             redirect: string;
+             redirect: string,
+             done: boolean;
+
   const params = `noJS&redirect=${redirect}`;
   const apiURL = `/api/todo/${id}`;
 </script>
-<section>
-  <form action="{apiURL}?{params}&_method=patch" method="post">
-    <button>Toogle done</button>
-  </form>
-  <form action="{apiURL}?{params}&_method=patch" method="post">
-    <input name="title" value={title} />
+
+<section class:done>
+  <form class="big" action="{apiURL}?{params}&_method=patch" method="post">
+    <input class="big" name="title" value={title} />
     <input name="description" value={description} />
     <button>change</button>
+  </form>
+  <form action="{apiURL}?{params}&_method=patch" method="post">
+    <button>Toogle done</button>
   </form>
   <form action="{apiURL}?{params}&_method=delete" method="post">
     <button>delete</button>
   </form>
 </section>
+
+<style lang="scss">
+@import 'fluid';
+@import 'colors';
+
+section {
+  @include fluid(gap, $very-small, $large, 10px, 18px ); 
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  max-width: 700px;
+  flex-grow: 1;
+  width: 100%;
+}
+
+form {
+  @include fluid(gap, $very-small, $large, 10px, 18px ); 
+
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  &.big {
+    grid-column: 1 / 3;
+  }
+}
+
+input {
+  width: clamp(100px, 80% ,500px);
+}
+</style>
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,11 @@
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const scssLibPath = path.join(__dirname, "/src/styles");
 
 /**@type {import('@sveltejs/kit').Config} */
 
@@ -8,7 +14,7 @@ const config = {
   /*Consult https://github.com/sveltejs/svelte-preprocess
      For more information about preprocessors */
 
-  preprocess: preprocess(),
+  preprocess: preprocess({ scss: { includePaths: [ scssLibPath ] } }),
 
   kit: {
     adapter:        adapter(),
